26332a6b76d0c02fa54db48b1e51724f1fa12c0f
Juan Cortés <juan.cortes@mad.vass.es>
2021-01-29 17:32:05 +0100

    Eslint and Prettier added

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+const STATE_OF_RULE = {
+  OFF: 'off',
+}
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': STATE_OF_RULE.OFF,
+  },
+};
